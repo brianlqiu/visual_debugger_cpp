771fc200523a5f19a912aba150c09cd5b58e1bb4
Brian Qiu <brianlqiu5@gmail.com>
2020-06-15 17:31:42 -0700

    added documentation/formatting

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 int main(int argc, char **argv) {
+    // Usage check
     if (argc != 2) {
         std::cerr << "Usage: ./visual_debugger <cpp_file>" << std::endl;
         return 1;
     }
 
-    std::cout << "Attemping to open " << *(argv + 1) << std::endl;
-    std::ifstream input;
-    input.open(*(argv + 1), std::ios::in);
+    // Opening input
+    std::cout << "Attemping to open " << *(argv + 1) << "..." << std::endl;
+    std::ifstream input(*(argv + 1), std::ios::in);
     if (!input.good()) {
-        std::cerr << "Error: " << *(argv + 1) << " is not a valid file" << std::endl;
+        std::cerr << "Error: " << *(argv + 1) << " is not a valid file"
+                  << std::endl;
         return 2;
     }
+    std::cout << *(argv + 1) << " was opened successfully" << std::endl;
+
+    // Creating output
 }
